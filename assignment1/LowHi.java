8865c128e394d4fddda0ce657f39137f1d1a4199
alux444 <aluux999@gmail.com>
2023-08-02 10:54:09 +1200

    new notes, implementation of mergesort

--- assignment1/LowHi.java
+++ assignment1/LowHi.java
@@ -5,14 +5,13 @@ import java.io.PrintWriter;
 import java.util.StringTokenizer;
 import java.util.Scanner;
 
- 
 public class LowHi {
-	
-	public static void main(String[] args) {
-    FastScanner in = new FastScanner();
- 		PrintWriter out = new PrintWriter(System.out);
 
-		int n = in.nextInt();
+  public static void main(String[] args) {
+    FastScanner in = new FastScanner();
+    PrintWriter out = new PrintWriter(System.out);
+
+    int n = in.nextInt();
     int a[] = new int[n];
     for (int i = 0; i < n; i++) {
       a[i] = in.nextInt();
@@ -25,28 +24,43 @@ public class LowHi {
       out.println(count_between(a, low, hi));
     }
     out.close();
-	}
+  }
 
   public static int count_between(int a[], int lo, int hi) {
-    // WRITE YOUR CODE HERE
-  }
+    int left = 1;
+    int right = a.length;
 
+    while (left <= right) {
+      int mid = (left + right) / 2;
+      
+
+      if (a[mid] < lo) {
+        left = mid + 1;
+      } else if (a[mid] > hi) {
+        right = mid - 1;
+      }
+    }
+
+    System.out.println(left + "/" + right);
+
+    return -1;
+  }
 
   static class FastScanner {
-      BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-      StringTokenizer st=new StringTokenizer("");
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer("");
 
-      String next() {
-        while (!st.hasMoreTokens())
-          try { 
-            st=new StringTokenizer(br.readLine());				              
-          } catch (IOException e) {}
-        return st.nextToken();
-      }
-      
-      int nextInt() {
-        return Integer.parseInt(next());
-      }
+    String next() {
+      while (!st.hasMoreTokens())
+        try {
+          st = new StringTokenizer(br.readLine());
+        } catch (IOException e) {
+        }
+      return st.nextToken();
+    }
+
+    int nextInt() {
+      return Integer.parseInt(next());
+    }
   }
 }
-
--- content/2 sorting/2 merge sort/MergeSort.java
+++ content/2 sorting/2 merge sort/MergeSort.java
@@ -1,5 +1,57 @@
 public class MergeSort {
+
+    public void mergeSort(int[] A, int start, int end) {
+        // we need to split the whole array into sortable pairs and recombine.
+
+        // base case for recursion.
+        if (start == end) {
+            return;
+        }
+
+        // recursively run after splitting array in half
+        int mid = (start + end) / 2;
+        mergeSort(A, start, mid);
+        mergeSort(A, mid + 1, end);
+
+        // recombine sorted arrays with a combining algorithm.
+        combineSorted(A, start, mid, end);
+    }
+
+    public void combineSorted(int[] A, int start, int mid, int end) {
+        System.out.println("combine sorting");
+        int[] B = new int[end - start + 1];
+        int leftPointer = start;
+        int rightPointer = mid + 1;
+
+        for (int i = 0; i < end - start + 1; i++) {
+            // if the left pointer has gone through first half of array:
+            if (leftPointer > mid) {
+                B[i] = A[rightPointer++];
+            }
+            // if the right pointer has gone through the other half already:
+            else if (rightPointer > end) {
+                B[i] = A[leftPointer++];
+            }
+            // otherwise, compare item from each side of array. increment pointer of used
+            // value.
+            else if (A[leftPointer] < A[rightPointer]) {
+                B[i] = A[leftPointer++];
+            } else {
+                // else, the right pointer is smaller. increment the right side.
+                B[i] = A[rightPointer++];
+            }
+        }
+
+        // copy array B to A
+        for (int j = 0; j < B.length; j++) {
+            A[start + j] = B[j];
+            System.out.println(B[j]);
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("hello");
+        int[] toSort = new int[] { 2, 1, 3 };
+        MergeSort mergeSorter = new MergeSort();
+        mergeSorter.mergeSort(toSort, 0, toSort.length - 1);
     }
 }
